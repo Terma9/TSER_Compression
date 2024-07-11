8f5eded4d6c5645aca89da2ad9754d238f95fcfe
Simon Kraus <simon.krausss@gmail.com>
2024-07-11 17:05:34 +0200

    Added run from bash to autoML

--- TSER Code/autogluon.py
+++ TSER Code/autogluon.py
@@ -1,3 +1,4 @@
+import argparse
 import pandas as pd 
 import numpy as np
 
@@ -5,84 +6,99 @@ import mlflow
 from tsfeatures import tsfeatures
 
 
-#Load already prepared data
-#train_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/Checking Out Data/data/prepared_data/AppliancesEnergy_TRAIN_ts_and_features.csv')
-train_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/data/prepared_data/Covid3Month_TRAIN_features.csv')
-
-#test_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/Checking Out Data/data/prepared_data/AppliancesEnergy_TEST_ts_and_features.csv')
-test_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/data/prepared_data/Covid3Month_TEST_features.csv')
-
-test_y = test_data["target"].values
-
-mlflow.set_tracking_uri("http://127.0.0.1:5001")
-
-#mlflow.set_experiment("BeijingPM25_Dataset")
-mlflow.set_experiment("ApplianacesEnergy_Dataset")
-#mlflow.set_experiment("IEEEPPG_Dataset")
-
-#Autogluon Training
 from autogluon.tabular import TabularDataset, TabularPredictor
-
-label = 'target'
-#print(train_data[label].describe())
-
-agluon_settings = {
-    "time_limit" : 120,
-    "presets" : "good_quality"
-}
-
-# Autogluon needs train_data and target in one df, target has to be named target
-predictor = TabularPredictor(label=label).fit(train_data, **agluon_settings)
-
-#Autogluon Prediction
-predictions = predictor.predict(test_data.drop(columns=["target"]))
-
-#print(predictor.evaluate(test_data, silent=True))
-#print(predictor.leaderboard(test_data))
-
-
-#See how well the model performed
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-# Calculate Mean Squared Error (MSE)
-mse = mean_squared_error(test_y, predictions)
-rmse = np.sqrt(mse)
+def run_agluon(source_path, experiment_name, run_name, time):
+    #Load already prepared data
+    #train_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/Checking Out Data/data/prepared_data/AppliancesEnergy_TRAIN_ts_and_features.csv')
+    train_data = pd.read_csv(source_path)
 
-# Calculate Mean Absolute Error (MAE)
-mae = mean_absolute_error(test_y, predictions)
+    #test_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/Checking Out Data/data/prepared_data/AppliancesEnergy_TEST_ts_and_features.csv')
+    test_data = pd.read_csv(source_path)
 
-# Calculate R-squared score
-r_squared = r2_score(test_y, predictions)
+    test_y = test_data["target"].values
 
-metrics = {
-    "RMSE" : rmse,
-    "MAE" : mae,
-    "R2" : r_squared
-}
+    mlflow.set_tracking_uri("http://127.0.0.1:5001")
 
-print("RMSE:", rmse)
-print("Mean Absolute Error:", mae)
-print("R-squared Score:", r_squared)
+    #mlflow.set_experiment("BeijingPM25_Dataset")
+    mlflow.set_experiment(experiment_name=experiment_name)
+    #mlflow.set_experiment("IEEEPPG_Dataset")
+
+    #Autogluon Training
+    
+
+    label = 'target'
+    #print(train_data[label].describe())
+
+    agluon_settings = {
+        "time_limit" : time,
+        "presets" : "good_quality"
+    }
+
+    # Autogluon needs train_data and target in one df, target has to be named target
+    predictor = TabularPredictor(label=label).fit(train_data, **agluon_settings)
+
+    #Autogluon Prediction
+    predictions = predictor.predict(test_data.drop(columns=["target"]))
+
+    #print(predictor.evaluate(test_data, silent=True))
+    #print(predictor.leaderboard(test_data))
 
 
-# Log with ml flow
-run_name = "Autogluon 5min f (z-normalisation) (run2#)"
+    #See how well the model performed
+    
+
+    # Calculate Mean Squared Error (MSE)
+    mse = mean_squared_error(test_y, predictions)
+    rmse = np.sqrt(mse)
+
+    # Calculate Mean Absolute Error (MAE)
+    mae = mean_absolute_error(test_y, predictions)
+
+    # Calculate R-squared score
+    r_squared = r2_score(test_y, predictions)
+
+    metrics = {
+        "RMSE" : rmse,
+        "MAE" : mae,
+        "R2" : r_squared
+    }
+
+    print("RMSE:", rmse)
+    print("Mean Absolute Error:", mae)
+    print("R-squared Score:", r_squared)
 
 
-with mlflow.start_run(run_name=run_name) as run:
+    # Log with ml flow
+    #run_name = "Autogluon 5min f (z-normalisation) (run2#)"
 
 
-    mlflow.sklearn.log_model(predictor, 'agluon_predictor')
+    with mlflow.start_run(run_name=run_name) as run:
 
-    # Log the automl params
-    mlflow.log_params(agluon_settings)
 
-    # Log fwiz features
-    mlflow.log_metrics({
-        "Started with Features" : train_data.shape[1],
-        })
+        mlflow.sklearn.log_model(predictor, 'agluon_predictor')
 
-    # Log the error metrics that were calculated during validation
-    mlflow.log_metrics(metrics)
+        # Log the automl params
+        mlflow.log_params(agluon_settings)
 
+        # Log fwiz features
+        mlflow.log_metrics({
+            "Started with Features" : train_data.shape[1],
+            })
+
+        # Log the error metrics that were calculated during validation
+        mlflow.log_metrics(metrics)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Agluon with specified parameters.")
+    parser.add_argument("source_path", type=str, help="Path to the source CSV file")
+    parser.add_argument("experiment_name", type=str, help="Name of the MLflow experiment")
+    parser.add_argument("run_name", type=str, help="Name of the MLflow run")
+    parser.add_argument("time", type=int, help="Time budget for Agluon in seconds")
+
+    args = parser.parse_args()
+    run_agluon(args.source_path, args.experiment_name, args.run_name, args.time)
 
--- TSER Code/flaml_and_fwiz.py
+++ TSER Code/flaml_and_fwiz.py
@@ -1,6 +1,8 @@
 
 # How to use:
 # To activate Fwiz: uncomment big code, add feature-after in metrics, use in flaml commented lines!
+
+import argparse
 import pandas as pd
 import numpy as np
 import mlflow
@@ -10,112 +12,127 @@ from featurewiz import FeatureWiz
 
 from flaml import AutoML
 
-#Load already prepared data
-#train_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/Checking Out Data/data/prepared_data/AppliancesEnergy_TRAIN_ts_and_features.csv')
-train_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/data/prepared_data/Covid3Month_TRAIN_features.csv')
-
-#test_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/Checking Out Data/data/prepared_data/AppliancesEnergy_TEST_ts_and_features.csv')
-test_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/data/prepared_data/Covid3Month_TEST_features.csv')
-
-test_y = test_data["target"].values
-
-
-mlflow.set_tracking_uri("http://127.0.0.1:5001")
-
-#mlflow.set_experiment("BeijingPM25_Dataset")
-mlflow.set_experiment("ApplianacesEnergy_Dataset")
-#mlflow.set_experiment("IEEEPPG_Dataset")
+# '/home/simon/TSER/Covid3Month_TRAIN_features.csv'
 
 
 
- # Automated feature Selection 
+def run_flaml(source_path, experiment_name, run_name, time):
 
 
-# no activated feature engg, takes too long, feature_engg='interactions'
-fwiz = FeatureWiz(transform_target=True, verbose=0, ) # see other parameters on doc
+    #Load already prepared data
+    #train_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/Checking Out Data/data/prepared_data/AppliancesEnergy_TRAIN_ts_and_features.csv')
+    train_data = pd.read_csv(source_path)
 
-train_data_fw , train_y_fw  = fwiz.fit_transform(train_data.drop(columns=['target']), train_data["target"])
+    #test_data = pd.read_csv('/home/sim/Desktop/TS Extrinsic Regression/Checking Out Data/data/prepared_data/AppliancesEnergy_TEST_ts_and_features.csv')
+    test_data = pd.read_csv(source_path)
 
-# apply same learned feature transformations on test data _-> nothing with current params??
-test_data_fw = fwiz.transform(test_data.drop(columns=['target']))
-
-
-### get list of selected features ###
-fwiz.features 
+    test_y = test_data["target"].values
 
 
+    mlflow.set_tracking_uri("http://127.0.0.1:5001")
 
-# AutoML Pipeline
-
-#Selecting Model and Tuning Hyperparameters with FLAML
-
-
-
-# rsme doesn't exist
-automl_settings = {
-    "task" : "regression",
-    "metric" : "mse",
-    "time_budget" :120,
-    "log_file_name" : "flaml_log_basic.log",
-
-}
-
-automl = AutoML()
-# for only flaml
-#automl.fit(X_train= train_data.drop(columns=['target']), y_train = train_data['target'], **automl_settings)
-automl.fit(X_train= train_data_fw, y_train = train_data['target'], **automl_settings)
-
-#See how well the model performed
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
-#for only flaml
-#predictions = automl.predict(test_data.drop(columns=['target']))
-predictions = automl.predict(test_data_fw)
-
-# Calculate Mean Squared Error (MSE)
-mse = mean_squared_error(test_y, predictions)
-rmse = np.sqrt(mse)
-
-# Calculate Mean Absolute Error (MAE)
-mae = mean_absolute_error(test_y, predictions)
-
-# Calculate R-squared score
-r_squared = r2_score(test_y, predictions)
-
-metrics = {
-    "RMSE" : rmse,
-    "MAE" : mae,
-    "R2" : r_squared
-}
-
-print("RMSE:", rmse)
-print("Mean Absolute Error:", mae)
-print("R-squared Score:", r_squared)
-
-
-#Ml-Flow Run Name
-run_name = "FLAML and fwiz 5min f "
-
-
-# Log with ml flow
-with mlflow.start_run(run_name=run_name) as run:
-
-
-    mlflow.sklearn.log_model(automl, 'automl')
-
-    # Log the automl params
-    mlflow.log_params(automl_settings)
-
-##"Selected Features" : train_x_flattend_fw.shape[1]
-    # Log fwiz features
-    mlflow.log_metrics({
-        "Started with Features" : train_data.shape[1],
-        "Selected Features" : train_data_fw.shape[1]
-        })
-
-    # Log the error metrics that were calculated during validation
-    mlflow.log_metrics(metrics)
+    #mlflow.set_experiment("BeijingPM25_Dataset")
+    mlflow.set_experiment(experiment_name=experiment_name)
+    #mlflow.set_experiment("IEEEPPG_Dataset")
 
 
 
+    # Automated feature Selection 
 
+
+    # no activated feature engg, takes too long, feature_engg='interactions'
+    fwiz = FeatureWiz(transform_target=True, verbose=0, ) # see other parameters on doc
+
+    train_data_fw , train_y_fw  = fwiz.fit_transform(train_data.drop(columns=['target']), train_data["target"])
+
+    # apply same learned feature transformations on test data _-> nothing with current params??
+    test_data_fw = fwiz.transform(test_data.drop(columns=['target']))
+
+
+    ### get list of selected features ###
+    fwiz.features 
+
+
+
+    # AutoML Pipeline
+
+    #Selecting Model and Tuning Hyperparameters with FLAML
+
+
+
+    # rsme doesn't exist
+    automl_settings = {
+        "task" : "regression",
+        "metric" : "mse",
+        "time_budget" : time,
+        "log_file_name" : "flaml_log_basic.log",
+
+    }
+
+    automl = AutoML()
+    # for only flaml
+    #automl.fit(X_train= train_data.drop(columns=['target']), y_train = train_data['target'], **automl_settings)
+    automl.fit(X_train= train_data_fw, y_train = train_data['target'], **automl_settings)
+
+    #See how well the model performed
+    from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+    #for only flaml
+    #predictions = automl.predict(test_data.drop(columns=['target']))
+    predictions = automl.predict(test_data_fw)
+
+    # Calculate Mean Squared Error (MSE)
+    mse = mean_squared_error(test_y, predictions)
+    rmse = np.sqrt(mse)
+
+    # Calculate Mean Absolute Error (MAE)
+    mae = mean_absolute_error(test_y, predictions)
+
+    # Calculate R-squared score
+    r_squared = r2_score(test_y, predictions)
+
+    metrics = {
+        "RMSE" : rmse,
+        "MAE" : mae,
+        "R2" : r_squared
+    }
+
+    print("RMSE:", rmse)
+    print("Mean Absolute Error:", mae)
+    print("R-squared Score:", r_squared)
+
+
+    #Ml-Flow Run Nam
+
+    # Log with ml flow
+    with mlflow.start_run(run_name=run_name) as run:
+
+
+        mlflow.sklearn.log_model(automl, 'automl')
+
+        # Log the automl params
+        mlflow.log_params(automl_settings)
+
+    ##"Selected Features" : train_x_flattend_fw.shape[1]
+        # Log fwiz features
+        mlflow.log_metrics({
+            "Started with Features" : train_data.shape[1],
+            "Selected Features" : train_data_fw.shape[1]
+            })
+
+        # Log the error metrics that were calculated during validation
+        mlflow.log_metrics(metrics)
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run FLAML with specified parameters.")
+    parser.add_argument("source_path", type=str, help="Path to the source CSV file")
+    parser.add_argument("experiment_name", type=str, help="Name of the MLflow experiment")
+    parser.add_argument("run_name", type=str, help="Name of the MLflow run")
+    parser.add_argument("time", type=int, help="Time budget for FLAML in seconds")
+
+    args = parser.parse_args()
+    run_flaml(args.source_path, args.experiment_name, args.run_name, args.time)
