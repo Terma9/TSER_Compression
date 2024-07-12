7388a7fd19e613471a343d5c378733ab24d21e5c
Simon Kraus <simon.krausss@gmail.com>
2024-07-12 12:41:11 +0200

    better names mlflow

--- TSER Code/flaml_experiments.py
+++ TSER Code/flaml_experiments.py
@@ -4,17 +4,20 @@ from flaml_and_fwiz import run_flaml
 path = '/home/simon/TSER/preparedData/'
 
 
-# Add List and Loop for doing more datasets at once
+# For Agluon change all occurences
+# For new Dataset, change ds_name
+# Maybe add parameter option to do all test at once! -> delete before that the old experiments!
 ds_name = 'AppliancesEnergy'
 
 
 
 def tsf_or_f():
+
     source_path_tsf = path + ds_name + '_TRAIN_None__ts_and_features.csv'
     source_path_f = path + ds_name + '_TRAIN_None__features.csv'
 
-    run_flaml(source_path_tsf, 'Test if tsf or f', 'Appliances Flaml tsf 15min', 15 * 60)
-    run_flaml(source_path_f, 'Test if tsf or f', 'Appliances Flaml f 15min', 15 * 60)
+    run_flaml(source_path_tsf, 'Test if tsf or f', 'tsf Appliances Flaml 15min', 15 * 60)
+    run_flaml(source_path_f, 'Test if tsf or f', 'f Appliances Flaml 15min', 15 * 60)
 
 
 
@@ -24,7 +27,7 @@ def best_time():
 
     times = [1 ,5, 15, 30, 60]
     for time in times:
-        run_flaml(source_path, 'Test best runtime', f'Appliances Flaml {time}min', time * 60)
+        run_flaml(source_path, 'Test best runtime', f'{time}min Appliances Flaml', time * 60)
 
 
 # run 3 times the same test
@@ -33,6 +36,26 @@ def test_deterministic():
 
     for i in range(3):
         run_flaml(source_path, 'Test deterministic', f'Run{i+1} Appliances Flaml 5min', 5 * 60)
+
+
+
+
+# I assume only f, 15min -> maybe change later
+
+
+# Do the runs, put on mlflow
+# Then calculte comp_ratios and save it as array -> transfer back, from that I can do bar chart!
+#def testrun_dct():
+
+
+
+
+
+
+
+
+
+
 
 
 
